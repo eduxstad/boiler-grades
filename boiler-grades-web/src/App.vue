1fe0f9293ae17701eb83d16bf29b936f32dd86bc
Erik Duxstad <eduxstad@gmail.com>
2022-03-14 14:03:08 -0500

    update link to spreadsheet and move attribution

--- boiler-grades-web/src/App.vue
+++ boiler-grades-web/src/App.vue
@@ -21,7 +21,7 @@
             online</a>
              or via 
             <a target="_blank" href="https://github.com/eduxstad/boiler-grades/raw/main/grades.xlsx">
-            download (7 MB)</a>
+            download (9 MB)</a>
             . It contains
             data from all courses at the West Lafayette campus with more than 10 students
             from Summer 2016 to Fall 2021. Some courses may not be shown if they only 
@@ -34,12 +34,9 @@
              (from the University of Minnesota), the team lead by
             <a target="_blank" href="https://waf.cs.illinois.edu/discovery/grade_disparity_between_sections_at_uiuc/">
             Wade Fagen-Ulmschneider</a>
-             (from the University of Illinois),
+             (from the University of Illinois), and 
             <a target="_blank" href="https://austingwalters.com/foia-requesting-100-universities/">
-            Austin G. Walters</a>
-            and
-            <a target="_blank" href="https://web.archive.org/web/20171127200536/https://easy-a.net/foia">
-            easy-a.net (archive link)</a>.
+            Austin G. Walters</a>. Fall 2021 data was provided by Joey Diesen.
             </p>
             <h2>Code</h2>
             <p>This website is open source. If you'd like to report or fix a bug, 
@@ -47,7 +44,7 @@
             <a target="_blank" href="https://github.com/eduxstad/boiler-grades"> 
             GitHub repository</a>.
             </p>
-            <p>Built with 🚂🔨 by <a target="_blank" href="https://github.com/eduxstad">eduxstad</a>. Fall 2021 Data Provided by Joey Diesen.</p>
+            <p>Built with 🚂🔨 by <a target="_blank" href="https://github.com/eduxstad">eduxstad</a>.</p>
             </v-container>
           </v-col>
         </v-row>
