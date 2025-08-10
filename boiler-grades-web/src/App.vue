b26e87d5fd1e3243b4120988c8ebc327c87cda9e
eduxstad <eduxstad@gmail.com>
2025-08-10 13:19:27 -0700

    spring 2025 update

--- boiler-grades-web/src/App.vue
+++ boiler-grades-web/src/App.vue
@@ -36,7 +36,7 @@
               Wade Fagen-Ulmschneider</a>
               (from the University of Illinois), and 
               <a target="_blank" href="https://austingwalters.com/foia-requesting-100-universities/">
-              Austin G. Walters</a>. Fall 2021, Spring 2022, Summer 2022, and Fall 2022 data was provided by Joey Diesen. Spring 2023 data was provided by Vineet Malik. Summer 2023, Fall 2023, Spring 2024, and Fall 2024 data was provided by Umbert Caseres. Fall 2023 data was supplemented by Leon.
+              Austin G. Walters</a>. Fall 2021, Spring 2022, Summer 2022, and Fall 2022 data was provided by Joey Diesen. Spring 2023 data was provided by Vineet Malik. Summer 2023, Fall 2023, Spring 2024, and Fall 2024 data was provided by Umbert Caseres. Fall 2023 data was supplemented by Leon. Spring 2025 data was provided by Abdullah Alsadi and Abdullah Alkazemi.
               </p>
               <h2>Code</h2>
               <p>This website is open source. If you'd like to report or fix a bug, 
